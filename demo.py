59ca79eeed838b8f81a682240d94877f44c55a5e
Pete Pongpeauk <kurtsiberg@gmail.com>
2023-10-05 00:05:46 -0400

    chore: update DMV url

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 from PlateAPI import API
+
 obj = API()
 
 while True:
-  plate_number = input("Plate Number: ")
-  print(f"Query Plate: {plate_number}")
-  query = obj.check_plate(plate=plate_number)
-  print(f"Available: {query}")
+    plate_number = input("Plate Number: ")
+    print(f"Query Plate: {plate_number}")
+    query = obj.check_plate(plate=plate_number)
+    print(f"Available: {query}")
--- multithreading_demo.py
+++ multithreading_demo.py
@@ -5,14 +5,20 @@ obj = API()
 words = open("words.txt", "r").read().splitlines()
 
 with open("./output.txt", "w") as output:
-  word_list = [word.strip() for word in words if word.strip().isalpha() and len(word.strip()) == 7]
-  def check(word):
-    print(f"Query: {word.upper()}")
-    query = obj.check_plate(plate=word)
-    print(f"Available: {query}\n")
-    if query:
-      output.write(f"{word.upper()}\n")
-  p = mp.Pool(16)
-  p.map(check, word_list)
-  p.close()
-  p.join()
+    word_list = [
+        word.strip()
+        for word in words
+        if word.strip().isalpha() and 4 <= len(word.strip()) <= 7
+    ]
+
+    def check(word):
+        print(f"Query: {word.upper()}")
+        query = obj.check_plate(plate=word)
+        print(f"Available: {query}\n")
+        if query:
+            output.write(f"{word.upper()}\n")
+
+    p = mp.Pool(16)
+    p.map(check, word_list)
+    p.close()
+    p.join()
